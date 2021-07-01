d4d5d63894c0abf854be7a6efcacc427dd242614
AlexanderAlves77 <vendas77.rj@gmail.com>
2021-07-01 09:26:24 -0300

    Criação do projeto gerenciador de tarefas

---
+++ src/main/kotlin/br/com/devaria/projetos/gerenciadortarefas/controllers/LoginController.kt
@@ -0,0 +1,42 @@
+package br.com.devaria.projetos.gerenciadortarefas.controllers
+
+import br.com.devaria.projetos.gerenciadortarefas.dtos.ErroDTO
+import br.com.devaria.projetos.gerenciadortarefas.dtos.LoginDTO
+import br.com.devaria.projetos.gerenciadortarefas.dtos.LoginRespostaDTO
+import br.com.devaria.projetos.gerenciadortarefas.utils.JWTUtils
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.lang.RuntimeException
+
+@RestController
+@RequestMapping( "api/login")
+class LoginController {
+
+    private val LOGIN_TESTE = "admin@admin.com"
+    private val SENHA_TESTE = "Admin1234@"
+
+    @PostMapping
+    fun efetuarLogin(@RequestBody dto : LoginDTO) : ResponseEntity<Any>{
+        try{
+            if(dto == null || dto.login.isNullOrBlank() || dto.login.isNullOrEmpty()
+                || dto.senha.isNullOrEmpty() || dto.senha.isNullOrBlank()
+                || dto.login != LOGIN_TESTE || dto.senha != SENHA_TESTE){
+                return ResponseEntity(ErroDTO(HttpStatus.BAD_REQUEST.value(),
+                    "Parametros de entradas inválidos"), HttpStatus.BAD_REQUEST)
+            }
+
+            val idUsuario = 1
+            val token = JWTUtils().gerarToken(idUsuario.toString())
+
+            val usuarioTeste = LoginRespostaDTO("Usuário Teste", LOGIN_TESTE, token)
+            return ResponseEntity( usuarioTeste, HttpStatus.OK)
+        }catch (e: Exception){
+            return ResponseEntity(ErroDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Não foi possível efetuar o login, tente novamente"), HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+}
---
+++ src/main/kotlin/br/com/devaria/projetos/gerenciadortarefas/filters/JWTAutorizadorFilter.kt
@@ -0,0 +1,41 @@
+package br.com.devaria.projetos.gerenciadortarefas.filters
+
+import br.com.devaria.projetos.gerenciadortarefas.authorization
+import br.com.devaria.projetos.gerenciadortarefas.bearer
+import br.com.devaria.projetos.gerenciadortarefas.models.Usuario
+import br.com.devaria.projetos.gerenciadortarefas.utils.JWTUtils
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import sun.plugin.liveconnect.SecurityContextHelper
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class JWTAutorizadorFilter(authenticationManager : AuthenticationManager, val jwtUtils: JWTUtils)
+    : BasicAuthenticationFilter(authenticationManager) {
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
+        val athorization = request.getHeader(authorization)
+
+        if(athorization != null && authorization.startsWith(bearer)){
+            val autorizado = getAuthentication(athorization)
+            SecurityContextHolder.getContext().authentication = autorizado
+        }
+
+        chain.doFilter(request, response)
+    }
+
+    private fun getAuthentication(athorization: String): UsernamePasswordAuthenticationToken {
+        val token = athorization.substring(7)
+        if(jwtUtils.isTokenValido(token)){
+            val idString = jwtUtils.getUsuarioId(token)
+            if(idString.isNullOrBlank() && idString.isNullOrEmpty()){
+                val usuario = Usuario(idString.toLong(), "Usuário Teste", "admin@admin.com", "Admin1234@")
+                return UsernamePasswordAuthenticationToken(usuario, null)
+            }
+        }
+        throw UsernameNotFoundException("Token informado não está válido, ou não tem uma informação de identificação do usuário.")
+    }
+}
